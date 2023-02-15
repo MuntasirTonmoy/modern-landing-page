d88ac21b87041341128abbb4c28c2842f8f5057a
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-02-15 17:23:28 +0600

    logo added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.html"],
   theme: {
     fontFamily: {
       body: "Poppins, sans-serif",
